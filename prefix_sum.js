6cc0a107cf87403320ff7a539e242842a3e404f6
devrushi <rushi1379@gmail.com>
2024-09-25 00:38:15 +0100

    Problem solving for Highest altitude

--- prefix_sum.js
+++ prefix_sum.js
@@ -30,3 +30,31 @@ let prefix_sum_optimized = (arr) => {
 }
 
 prefix_sum_optimized([1,2,3,4,5,6,7,8,9,10]); // [1,3,6,10,15,21,28,36,45,55]
+
+//Problem solving 
+//Find the Highest Altitude
+//There is a biker going on a road. The road trip consists of n + 1 points at different altitudes.
+// The biker starts his trip on point 0 with altitude equal 0.
+// You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n).
+// Return the highest altitude of a point.
+
+let highestAltitude = (gain) => {
+    let maxValue = 0;
+    let sum = [];
+    
+    for (let i = 0; i < gain.length; i++){
+        if(i === 0){
+            sum[i] = gain[i];
+           
+        }else{
+            sum[i] = sum[i-1] + gain[i];
+            
+        }
+        if(sum[i] > maxValue){
+            maxValue = sum[i];
+        }
+    }
+    console.log(maxValue);
+}
+
+highestAltitude([-5,1,5,0,-7]); // 1
